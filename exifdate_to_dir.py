50f5f772299b411ea11769e8aa25024485cff534
Alex <298750+alexantao@users.noreply.github.com>
2019-09-18 11:13:03 -0300

    useing PyExiTtool
    Changed to use PyExifTool to get RAW files information as well.

--- exifdate_to_dir.py
+++ exifdate_to_dir.py
@@ -1,68 +1,40 @@
 #! /usr/bin/env python3
 
 import glob
-
-from PIL import Image
-from PIL.ExifTags import TAGS,GPSTAGS
+import exiftool
 import os
 import datetime
 from argparse import ArgumentParser
 from shutil import move
 
-#     This program analyses the EXIF data from a photo file,
-#   takes the date it was shot and moves the file to a subdirectory
-#   structure like: YEAR/MONTH/DAY, acording to the options passed
-#
-#     It is good to structure your photo library that programs like
-#   Digikam is watching.
+# the field to extract from photo files
+EXIF_DATE_FIELD = "DateTimeOriginal"
 
-#-------------------------------------------------------------------
-# Function to extract aoo EXIF information from a file.
-# Returns en EMPTY dictionary if none found
-def get_exif_data(path):
-    tags = {}
-    if os.path.isfile(path):
-        image = Image.open(path)
-        try:
-            image_info = image._getexif()
-            for tag, value in image_info.items():
-                key = TAGS.get(tag, tag)
-                tags[key] = value
-        finally:
-            return tags
-    else:
-        return tags
-#-------------------------------------------------------------------
-# Main program function
 def run(source, output_dir):
 
-# 1. Gera a lista dos arquivos passados
-# 2. Para cada arquivo:
-#   2.1 - Lê as informações no EXIF do arquivo
-#   2.2 - Recupera a data
-#   2.3 - Cria diretório do ano/mes/dia
-#   2.4 - Move o arquivo para o diretório (verificar se já existe lá !)
-
-    print("Source:", source)
-
     for dir_entry in source:
-        if os.path.isdir(dir_entry):    # item in list is directory
+        if os.path.isdir(dir_entry):  # item in list is directory
             dir_entry = os.path.join(dir_entry, "*")
 
-        file_list = glob.glob(dir_entry)       # generate a list of the source
+        file_list = glob.glob(dir_entry)  # generate a list of the source
         file_list.sort()  # lets just sort the list
 
         for original_photo in file_list:
             print("----------------------\nTesting: ", original_photo)
             photo_basename = os.path.basename(original_photo)
-            tags = get_exif_data(original_photo)
-            if len(tags) > 0:
-                try: # first, separate year, month and date from EXIT
-                    original_date_str = tags['DateTimeOriginal']                                        # get original EXIF date
-                    date_of_photo = datetime.datetime.strptime(original_date_str, '%Y:%m:%d %H:%M:%S')  # transform to date obj
-                    photo_year = date_of_photo.year                                                     # separate year
-                    photo_month = date_of_photo.month                                                   # separate month
-                    photo_day = date_of_photo.day                                                       # separate day
+
+            try:
+                # get Metadata from file
+                with exiftool.ExifTool() as metadata_tool:
+                    original_date_str = metadata_tool.get_tag(EXIF_DATE_FIELD, original_photo)
+
+                if original_date_str != None:
+
+                    date_of_photo = datetime.datetime.strptime(original_date_str,
+                                                               '%Y:%m:%d %H:%M:%S')  # transform to date obj
+                    photo_year = date_of_photo.year  # separate year
+                    photo_month = date_of_photo.month  # separate month
+                    photo_day = date_of_photo.day  # separate day
 
                     # lets create directory struct as ordered
                     final_path = os.path.join(output_dir, str(photo_year))
@@ -75,12 +47,10 @@ def run(source, output_dir):
                     # create Path
                     print("Moving : ", photo_basename, " -> ", destination_file)
                     os.makedirs(os.path.dirname(destination_file), exist_ok=True)           # Directory does nor exist, create
-                    #move(original_photo, destination_file)
-
-                except:  #photos whitout EXIF information or DateTimeOriginal are ignored
-                    print('File: "', original_photo, '" does not have Date/Time information.')
-                    continue
-
+                    move(original_photo, destination_file)
+            except:  #photos whitout EXIF information or DateTimeOriginal are ignored
+                print('File: "', original_photo, '" does not have Date/Time information.')
+                continue
 
 
 # Usage: ./exifdate_to_dir.py [options] <source> <output_dir>
