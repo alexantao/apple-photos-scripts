2bc249e2f23208307f734c1a64f95b573d59e7e0
alexantao <alex@familiaturista.com.br>
2019-09-18 11:30:56 -0300

    POP8 corrections

--- exifdate_to_dir.py
+++ exifdate_to_dir.py
@@ -10,8 +10,8 @@ from shutil import move
 # the field to extract from photo files
 EXIF_DATE_FIELD = "DateTimeOriginal"
 
-def run(source, output_dir):
 
+def run(source, output_dir):
     for dir_entry in source:
         if os.path.isdir(dir_entry):  # item in list is directory
             dir_entry = os.path.join(dir_entry, "*")
@@ -38,28 +38,29 @@ def run(source, output_dir):
 
                     # lets create directory struct as ordered
                     final_path = os.path.join(output_dir, str(photo_year))
-                    if args.day or args.month:       # create full structure
+                    if args.day or args.month:  # create full structure
                         final_path = os.path.join(final_path, str(photo_month))
                     if args.day:
                         final_path = os.path.join(final_path, str(photo_day))
 
-                    destination_file = os.path.join(final_path, photo_basename)             # this is the final pathname
+                    destination_file = os.path.join(final_path, photo_basename)  # this is the final pathname
                     # create Path
                     print("Moving : ", photo_basename, " -> ", destination_file)
-                    os.makedirs(os.path.dirname(destination_file), exist_ok=True)           # Directory does nor exist, create
+                    os.makedirs(os.path.dirname(destination_file), exist_ok=True)  # Directory does nor exist, create
                     move(original_photo, destination_file)
-            except:  #photos whitout EXIF information or DateTimeOriginal are ignored
+            except:  # photos whitout EXIF information or DateTimeOriginal are ignored
                 print('File: "', original_photo, '" does not have Date/Time information.')
                 continue
 
 
 # Usage: ./exifdate_to_dir.py [options] <source> <output_dir>
 if __name__ == '__main__':
-
     parser = ArgumentParser()
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Prints all info about processing')
-    parser.add_argument('-y', '--year', action='store_true', default=True, help='Separate Photos by year (default True).')
-    parser.add_argument('-m', '--month', action='store_true', default=False, help='Separate Photos by month, inside year.')
+    parser.add_argument('-y', '--year', action='store_true', default=True,
+                        help='Separate Photos by year (default True).')
+    parser.add_argument('-m', '--month', action='store_true', default=False,
+                        help='Separate Photos by month, inside year.')
     parser.add_argument('-d', '--day', action='store_true', default=False, help='Separate Photos by day, inside month.')
 
     parser.add_argument('source', nargs='*', help='Source files pattern or dir of the photos that will be processed')
